b12d0a3a025fbbb033a602ac06afaa0650f2714b
brshomh <Brshomh.1@gmail.com>
2026-01-03 16:38:23 +0400

    Updated sound.ts

---
+++ services/sound.ts
@@ -0,0 +1,6 @@
+// services/sound.ts
+export const sounds = {
+  playClick: () => console.log("Sound: Click"), // استبدلها بملف صوتي حقيقي لاحقاً
+  playConnect: () => console.log("Sound: Connect"),
+  playError: () => console.log("Sound: Error"),
+};
