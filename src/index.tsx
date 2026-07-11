073fe6ab33789d0453e60b551e1d1a5a16b691c8
brshomh <Brshomh.1@gmail.com>
2026-07-12 00:30:00 +0300

    Refactor App component rendering method

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-function App() {
-  return <h1>Hello World!</h1>
-}
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('Failed to find the root element');
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+const root = ReactDOM.createRoot(rootElement);
+root.render(React.createElement(React.StrictMode, null, React.createElement(App)));
